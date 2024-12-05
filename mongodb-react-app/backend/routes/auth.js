9b7e91007abc6c821234ced9d98db1ae4fce6c2a
olha-dev-fullstack <olha.shevel.dev@gmail.com>
2024-12-05 18:41:52 +0200

    add pagination and authorization logic

--- mongodb-react-app/backend/routes/auth.js
+++ mongodb-react-app/backend/routes/auth.js
@@ -1,42 +1,64 @@
-const Router = require('express').Router;
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcryptjs');
+const Router = require("express").Router;
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+
+const db = require("../db");
 
 const router = Router();
 
 const createToken = () => {
-  return jwt.sign({}, 'secret', { expiresIn: '1h' });
+  return jwt.sign({}, "secret", { expiresIn: "1h" });
 };
 
-router.post('/login', (req, res, next) => {
+router.post("/login", (req, res, next) => {
   const email = req.body.email;
   const pw = req.body.password;
-  // Check if user login is valid
-  // If yes, create token and return it to client
-  const token = createToken();
-  // res.status(200).json({ token: token, user: { email: 'dummy@dummy.com' } });
-  res
-    .status(401)
-    .json({ message: 'Authentication failed, invalid username or password.' });
+  db.getDb()
+    .collection("users")
+    .findOne({ email: email })
+    .then((userDoc) => {
+      return bcrypt.compare(pw, userDoc.password);
+    })
+    .then((result) => {
+      if (!result) {
+        throw Error();
+      }
+      const token = createToken();
+      res
+        .status(200)
+        .json({ message: "Authentication succeeded.", token: token });
+    })
+    .catch((err) => {
+      res.status(401).json({
+        message: "Authentication failed, invalid username or password.",
+      });
+    });
 });
 
-router.post('/signup', (req, res, next) => {
+router.post("/signup", (req, res, next) => {
   const email = req.body.email;
   const pw = req.body.password;
   // Hash password before storing it in database => Encryption at Rest
   bcrypt
     .hash(pw, 12)
-    .then(hashedPW => {
+    .then((hashedPW) => {
       // Store hashedPW in database
-      console.log(hashedPW);
-      const token = createToken();
-      res
-        .status(201)
-        .json({ token: token, user: { email: 'dummy@dummy.com' } });
+      db.getDb()
+        .collection("users")
+        .insertOne({ email: email, password: hashedPW })
+        .then((result) => {
+          const token = createToken();
+          res.status(201).json({ token: token, user: { email: email } });
+          console.log(result);
+        })
+        .catch((err) => {
+          console.log(err);
+          res.status(500).json({ message: "Creating the user failed." });
+        });
     })
-    .catch(err => {
+    .catch((err) => {
       console.log(err);
-      res.status(500).json({ message: 'Creating the user failed.' });
+      res.status(500).json({ message: "Creating the user failed." });
     });
   // Add user to database
 });
